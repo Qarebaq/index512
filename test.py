021ecd6327a88bcb6443c039e8888046c6cb5b58
Amir Mohammad Mohammadi <95334969+Qarebaq@users.noreply.github.com>
2024-10-29 01:02:20 +0330

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+print ("hello world!")
+user= int(input ("wich user you want to continue with that? one or two write 1 or 2"))
+
+
+litr = input ("how many letrers do you want?")
+
+if user == 1:
+    print(int(litr)* 1500)
+if user==2:
+    print(int(litr)* 2000 )
+else:
+    print("enter correct number")
